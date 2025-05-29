bfd4da7425e674d2e7773379a558cccc70124691
NicolasWiedel <nicolaswiedel@googlemail.com>
2025-05-29 16:52:47 +0200

    2.1.  Hinzufügen eines ControlPanels
    Listet die Beispiele auf und ermöglicht das Starten über einen Button

--- lwjgl2/src/main/java/com/nicolas/gdxbeispiel/lwjgl2/GDXBeispielLauncher.java
+++ lwjgl2/src/main/java/com/nicolas/gdxbeispiel/lwjgl2/GDXBeispielLauncher.java
@@ -6,15 +6,38 @@ import com.badlogic.gdx.utils.reflect.ClassReflection;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class GDXBeispielLauncher extends JFrame {
 
     private static final int WIDTH = 1280;
     private static final int HEIGHT = 720;
+    private static final int CELL_WIDTH = 200;
+    private static final int CANVAS_WIDTH = WIDTH - CELL_WIDTH;
 
     private LwjglAWTCanvas lwjglAWTCanvas;
+    private JList<String> beispielList;
+    private JPanel controlPanel;
 
     public GDXBeispielLauncher() throws HeadlessException {
+        init();
+    }
+
+    private void init(){
+        createControlPanel();
+
+        Container container = getContentPane();
+        container.add(controlPanel, BorderLayout.WEST);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                if (lwjglAWTCanvas != null){
+                    lwjglAWTCanvas.stop();
+                }
+            }
+        });
+
         setTitle(GDXBeispielLauncher.class.getSimpleName());
         setMinimumSize(new Dimension(WIDTH, HEIGHT));
         setSize(WIDTH, HEIGHT);
@@ -23,8 +46,57 @@ public class GDXBeispielLauncher extends JFrame {
         setLocationRelativeTo(null);
         pack();
         setVisible(true);
+    }
 
-        launchBeispiel("com.nicolas.gdxbeispiel.BasicsInputPolling");
+    private void createControlPanel(){
+        controlPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.gridx = 0;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.VERTICAL;
+        c.weighty = 1;
+
+        beispielList = new JList<String>(
+            new String[]{"BasicsInputPolling"}
+        );
+        beispielList.setFixedCellWidth(CELL_WIDTH);
+        beispielList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        beispielList.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if(e.getClickCount() == 2){
+                    launchSelectedBeispiel();
+                }
+            }
+        });
+        JScrollPane scrollPane = new JScrollPane(beispielList);
+        controlPanel.add(scrollPane, c);
+
+        c.gridx = 0;
+        c.gridy = 1;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weighty = 0;
+
+        JButton launchButton = new JButton("Starte LibGDX Beispiel");
+        launchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                launchSelectedBeispiel();
+            }
+        });
+
+        controlPanel.add(launchButton, c);
+    }
+
+    private void launchSelectedBeispiel(){
+        String beispielName = beispielList.getSelectedValue();
+
+        if (beispielName == null || beispielName.isEmpty()){
+            System.out.println("Kein Beispiel gewählt!");
+            return;
+        }
+        launchBeispiel("com.nicolas.gdxbeispiel." + beispielName);
     }
 
     private void launchBeispiel(String name){
@@ -46,7 +118,7 @@ public class GDXBeispielLauncher extends JFrame {
             throw new RuntimeException("Kann das Beispiel mit Namen: " + name + " nicht starten!");
         }
         lwjglAWTCanvas = new LwjglAWTCanvas(beispiel);
-        lwjglAWTCanvas.getCanvas().setSize(WIDTH, HEIGHT);
+        lwjglAWTCanvas.getCanvas().setSize(CANVAS_WIDTH, HEIGHT);
         container.add(lwjglAWTCanvas.getCanvas(), BorderLayout.CENTER);
         pack();
     }
