8a3947f5f2252ca5480eb5f7c661760a3c996205
NicolasWiedel <nicolaswiedel@googlemail.com>
2025-08-03 20:46:24 +0200

    9. TexturePacker

---
+++ lwjgl2/src/main/java/com/nicolas/gdxbeispiel/lwjgl2/AssetPacker.java
@@ -0,0 +1,14 @@
+package com.nicolas.gdxbeispiel.lwjgl2;
+
+public class AssetPacker {
+
+    public static final boolean DRAW_DEBUG_OUTLINE = true;
+
+    public static final String RAW_ASSETS_PATH = "lwjgl2/assets-raw";
+    public static final String ASSETS_PATH = "assets";
+
+
+    public static void main(String[] args) {
+
+    }
+}
